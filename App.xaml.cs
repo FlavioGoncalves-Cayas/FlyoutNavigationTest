e5944bb38f071dce911b0460e98810ba38781994
Flavio Goncalves <f.goncalves@cayas.de>
2023-10-16 14:50:02 +0200

    Initial commit

---
+++ App.xaml.cs
@@ -0,0 +1,49 @@
+﻿namespace FlyoutNavigationTest;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		FlyoutPage flyout = null;
+        var toAnotherPageCommand = new Command(() => flyout.Detail.Navigation.PushAsync(new ContentPage
+        {
+            Title = "Another page"
+        }));
+        var changePageCommand = new Command(() => flyout.Detail = new NavigationPage(new ContentPage
+		{
+			Title = "That page",
+            Content = new StackLayout
+            {
+                Children = {
+                        new Label { Text = "That page" },
+                        new Button { Text = "To another page", Command = toAnotherPageCommand }
+                    }
+            }
+        }));
+		MainPage = flyout = new FlyoutPage
+		{
+			Flyout = new ContentPage()
+			{
+				Title = "Menu",
+				Content = new StackLayout
+				{
+					Children = {
+						new Button
+						{
+							Text = "Change Page",
+							Command = changePageCommand
+						}
+					}
+				}
+			},
+			Detail = new NavigationPage(new ContentPage()
+			{
+				Title = "This Page",
+				Content = new Label { Text = "This page" }
+            })
+		};
+	}
+}
+
---
+++ Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace FlyoutNavigationTest;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
+
